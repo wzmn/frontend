7de8b4a20bce131fb9bc1edb2433b072b172fef9
anish_cogncise <anishb@cogncise.com>
2023-10-05 07:39:57 +0530

    conditionaly rendering layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "./navbar";
+import { useLocation } from "@reach/router";
+
 import "../styles/global.css";
 const Layout = ({ children }: { children: JSX.Element }) => {
+  const location = useLocation();
+
   return (
     <div className="layout">
-      <Navbar />
+      {location.pathname !== "/login/" && <Navbar />}
       {children}
       This is a footer
     </div>
