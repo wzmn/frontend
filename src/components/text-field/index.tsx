2e92b7e87487ba548e3eadf23e3b414cfe1e6f04
anish_cogncise <anishb@cogncise.com>
2023-10-26 08:19:22 +0530

    created new Input component as TextField

---
+++ src/components/text-field/index.tsx
@@ -0,0 +1,41 @@
+import React, { forwardRef } from "react";
+import * as styles from "./styles.module.scss";
+
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  //...add your custom types here
+  asterisk?: boolean;
+  errorMessage?: string;
+  title: string;
+}
+
+const TextField = forwardRef<HTMLInputElement, Omit<Props, "placeholder">>(
+  (props, ref) => {
+    const { asterisk, errorMessage = "", title } = props;
+    return (
+      <div className={styles.textField}>
+        <div
+          className={`${styles.inputCont} ${
+            errorMessage !== "" && styles.error
+          }`}
+        >
+          <input placeholder="" ref={ref} {...props} />
+          <span
+            onClick={(e) =>
+              (
+                (e.target as HTMLElement)
+                  .previousElementSibling as HTMLInputElement
+              ).focus()
+            }
+            className={styles.placeholder}
+          >
+            {title}
+            {asterisk && <span className="text-red-500">*</span>}
+          </span>
+        </div>
+        <span className={styles.errorMessage}>{errorMessage}</span>
+      </div>
+    );
+  }
+);
+
+export default TextField;
