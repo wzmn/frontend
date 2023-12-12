8c258bb9b41067bfd3fb829057e993f76980949b
Wiseman Fernandes <wizyman@gmail.com>
2023-12-12 11:05:30 +0530

    cherry pick

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -38,7 +38,7 @@ const Modal = ({ options, children }: any) => {
                                 {options.title ? options.title : "Modal title"}
                             </h3>
                             <button type="button" onClick={() => {
-                                options.onClose()
+                                options.onClose ? options.onClose() : '';
                                 setOpen(!open)
                             }} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm h-8 w-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="timeline-modal">
                                 <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
