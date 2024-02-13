b7cb6e259bde7c8439a0252448c1babc31009d8a
anish_cogncise <anishb@cogncise.com>
2024-02-13 15:01:37 +0530

    minor change

--- src/schema/add-product-schema.ts
+++ src/schema/add-product-schema.ts
@@ -7,4 +7,9 @@ const addProductSchema = object({
   supplier_company: string().trim().required("required"),
   price: string().trim().required("required"),
   discount_price: string().trim().required("required"),
+  cert_company: string().trim().required("required"),
+  start_date: string().trim().required("required"),
+  end_date: string().trim().required("required"),
+  regions: string().trim().required("required"),
+  description: string().trim().required("required"),
 });
